1e10be072963d3e8444d3050217d5880cb02c9bf
Lydia Bingamon <lydiabingamon@cedarville.edu>
2025-03-03 15:52:48 -0700

    changed testing method

--- crosswordPlayer/vite.config.ts
+++ crosswordPlayer/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: 'src/tests/setup.js', // assuming the test folder is in the root of our project
+  }
+} as UserConfig)

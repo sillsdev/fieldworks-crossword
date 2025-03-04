95c0accc84eb6cf89c38075ef533dbd6214bc29a
Lydia Bingamon <lydiabingamon@cedarville.edu>
2025-03-03 17:54:50 -0700

    started backend work grabbing data from dictionary

---
+++ crosswordPlayer/crosswordGenerator/server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+});
+app.get("/fetch-data", async (req, res) => {
+    console.log("Fetching data from external API...");
+    const apiUrl = 'http://localhost:49279/api/localProjects';
+    
+    try {
+        const apiResponse = await axios.get(apiUrl);
+        // Print the data from the API
+        console.log(apiResponse.data[0].apiEndpoint);
+        console.log('Data from API:', apiResponse.data);
+    } catch (error) {
+        console.error("Error fetching data:", error.message);
+    }
+});
+
+app.get("/fetch-words", async (req, res) => {
+    console.log("Fetching words...");
+    const apiUrl = 'http://localhost:49279/api/mini-lcm/FwData/sena-3/entries?count=-1';
+    
+    try {
+        const apiResponse = await axios.get(apiUrl);
+        // console.log('Data from API:', apiResponse.data);
+        const filteredData = apiResponse.data.filter(entry => {
+            let word = entry.citationForm.seh || entry.lexemeForm.seh;
+            return validateWord(word);
+        });
+        let my10Words = chooseRandomWords(filteredData, 10);
+        my10Words.forEach(entry => {
+            // print {word - definition}
+            console.log((entry.citationForm.seh || entry.lexemeForm.seh) + " - " + (entry.senses[0].definition.en || entry.senses[0].gloss.en));
+        });
+    } catch (error) {
+        console.error("Error fetching data:", error.message);
+    }
+});
+
+function chooseRandomWords(dictionaryWords, numWords) {
+    const randomWords = [];
+    const usedIndices = new Set();
+    while (randomWords.length < numWords) {
+        const randomIndex = Math.floor(Math.random() * dictionaryWords.length);
+        // make sure the index hasn't been picked already
+        if (!usedIndices.has(randomIndex)) {
+            usedIndices.add(randomIndex);
+            randomWords.push(dictionaryWords[randomIndex]);
+        }
+    }
+    return randomWords;
+}
+
+// make sure word does not contain spaces or numbers
+// makes sure it is between 4 and 10 characters long
+// this can be added to if we determine more validation is needed
+function validateWord(word) {
+    return word.length <= 10 && word.length >= 4 && !/[\s\d]/.test(word);
+}

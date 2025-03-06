2538e34914760129f2b4a177891db85e9ed5f5ea
Hannah VanGeest <109290794+hvangeest@users.noreply.github.com>
2025-03-06 13:17:40 -0500

    Display languages (#8)
    * displays languages
    
    * segmented code

--- crosswordPlayer/src/App.tsx
+++ crosswordPlayer/src/App.tsx
@@ -1,9 +1,8 @@
-import { Container, Box, Typography, Button, Menu, MenuItem } from '@mui/material';
+import { Container, Box, Typography, Button } from '@mui/material';
 import CrosswordBoard from './components/Crossword/CrosswordBoard';
 import CrosswordClues from './components/Crossword/CrosswordClues';
 import { useCrossword } from './hooks/useCrossword';
-import useLanguageGenerator from './hooks/useLanguageGenerator';
-import { useState } from 'react';
+import LanguageSelector from './components/LanguageSelector';
 
 function App() {
   const { 
@@ -15,40 +14,21 @@ function App() {
     isActiveCell 
   } = useCrossword();
 
-  const { fetchLanguages, languages } = useLanguageGenerator(); 
-
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const [selectedLanguage, setSelectedLanguage] = useState<string>("Select Language");
-  const open = Boolean(anchorEl);
-
-  const handleLanguageClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
-    await fetchLanguages();
-    setAnchorEl(event.currentTarget);
-  };
-  
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  
-  const handleLanguageSelect = (projectName: string, languageCode: string) => {
-    setSelectedLanguage(projectName);
-    handleClose();
-    console.log(`Selected project: ${projectName}, language code: ${languageCode}`);
-  };
-
   return (
     <Container 
       sx={{ 
         width: '100vw',
         height: '100vh', 
         display: 'flex', 
-        justifyContent: 'center',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
         alignItems: 'center',
         px: { xs: 2, sm: 4 },
         py: 2,
-        pt: 8
+        pt: 4
       }}
     >
+      <LanguageSelector />
       <Box sx={{ 
         width: '100%',
         maxWidth: 600,
@@ -94,35 +74,16 @@ function App() {
           </Button>
         </Box>
       </Box>
-      <Box>
-        <Button
-          onClick={handleLanguageClick}
-          variant="outlined"
-        >
-          {selectedLanguage}
-        </Button>
-        <Menu
-          id="language-menu"
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-        >
-          {languages.map((language) => (
-            <MenuItem
-              key={language.projectName}
-              onClick={() => handleLanguageSelect(language.projectName, language.languageCode)}
-            >
-              {language.languageCode}
-            </MenuItem>
-          ))}
-        </Menu>
-      </Box>
-      { <CrosswordClues 
+      
+      {/* Crossword clues */}
+      <Box sx={{ width: '100%', maxWidth: 600, mt: 3 }}>
+        <CrosswordClues 
           onClueClick={handleClueClick}
           activeClue={activeClue}
-      /> }
+        />
+      </Box>
     </Container>
-  )
+  );
 }
 
 export default App;
